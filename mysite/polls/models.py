7d10849c36a5763be84f4d206f604bd5eb6f2d38
SinKhLeft <102644622+SinKhLeft@users.noreply.github.com>
2025-04-15 15:22:45 +0900

    20250415 학습내용 정리 및 실습파일

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"제목: {self.question_text} 날짜 : {self.pub_date}"
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+    
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, word.")
+
+def some_url(request):
+    return HttpResponse("Hello, some.")
