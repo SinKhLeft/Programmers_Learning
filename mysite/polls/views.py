1907d2946ed3c0dea6f8690c47d7bc3ca7c210e8
SinKhLeft <102644622+SinKhLeft@users.noreply.github.com>
2025-04-16 17:02:33 +0900

    20250416 학습파일 커밋

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,38 @@
+from .models import *
 from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
 
 def index(request):
-    return HttpResponse("Hello, word.")
+    question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([q.question_text for q in last_question_list])
+    context = {'question_list': question_list}
+    return render(request, 'index.html', context)
 
-def some_url(request):
-    return HttpResponse("Hello, some.")
+def detail(request, question_id):
+    question = get_object_or_404(Question,id=question_id) #Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'detail.html', context)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    try:
+        selected_choice = question.choice_set.get(id=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'detail.html', {
+            'question': question,
+            'error_message': "선택된 값이 없습니다.",
+            #'error_message': f"선택된 값이 없습니다. id ={request.POST['choice']}",
+        })
+    else:
+        selected_choice.vote = F('vote')+1
+        selected_choice.save()
+        #return HttpResponseRedirect(reverse('questions:index'))
+        return HttpResponseRedirect(reverse('questions:results',args=(question.id,)))
+    
+def results(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    context = {'question': question}
+    return render(request, 'results.html', context)
